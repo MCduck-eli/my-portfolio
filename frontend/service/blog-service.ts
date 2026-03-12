f1e66bf8e074953a83733285a4f032ee7ca92a06
Eldor Khalikov <eldorabdukhalikov74@gmail.com>
2026-03-13 01:17:41 +0500

    change blog-service

--- frontend/service/blog-service.ts
+++ frontend/service/blog-service.ts
@@ -1,12 +1,19 @@
 import axios from "axios";
 import { IBlogTypes } from "@/types/blog-types";
 
-const BASE_URL = `${process.env.NEXT_PUBLIC_URL}/blog`;
+const BASE_URL = `${process.env.NEXT_PUBLIC_API_URL}/blog`;
 
 class BlogServiceClass {
     async getAllBlog(): Promise<IBlogTypes[]> {
-        const { data } = await axios.get<IBlogTypes[]>(BASE_URL);
-        return data;
+        try {
+            const { data } = await axios.get<IBlogTypes[]>(
+                `${BASE_URL}?t=${new Date().getTime()}`,
+            );
+            return data;
+        } catch (error) {
+            console.error("Error fetching blogs:", error);
+            return [];
+        }
     }
 
     async create(blogData: any) {
@@ -18,7 +25,6 @@ class BlogServiceClass {
         if (blogData.imageFile) {
             formData.append("image", blogData.imageFile);
         }
-
         const { data } = await axios.post(BASE_URL, formData, {
             headers: {
                 "Content-Type": "multipart/form-data",
@@ -28,7 +34,10 @@ class BlogServiceClass {
     }
 
     async update(id: string, dto: Partial<IBlogTypes>): Promise<IBlogTypes> {
-        const { data } = await axios.put<IBlogTypes>(`${BASE_URL}/${id}`, dto);
+        const { data } = await axios.patch<IBlogTypes>(
+            `${BASE_URL}/${id}`,
+            dto,
+        );
         return data;
     }
 
